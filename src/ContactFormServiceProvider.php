3dd45b9d9b7998e90ae0bb6b6a3a7781f0880e4e
onomatopoeia69 <carljameslangres@gmail.com>
2025-06-18 08:21:57 +0800

    updating it to component

--- src/ContactFormServiceProvider.php
+++ src/ContactFormServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Onoma\Contactform;
 
+
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Onoma\Contactform\Http\View\Components\EmailComponent;
 
 class ContactFormServiceProvider extends ServiceProvider
 {
@@ -24,6 +27,7 @@ class ContactFormServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'contactform');
         $this->loadMigrationsFrom(__DIR__ . "/../database/migrations");
-         $this->publishes([__DIR__ . '/../config/config.php' => config_path('contactform.php')],'contactform-config');  
+        $this->publishes([__DIR__ . '/../config/config.php' => config_path('contactform.php')],'contactform-config');
+        Blade::component('email-template',EmailComponent::class);
     }
 }
